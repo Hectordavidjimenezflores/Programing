824a76b50d08b06328863a48f979a8d5e5fb4db4
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-13 15:35:02 -0500

    Update calc.h

--- homework/homework4/calc.h
+++ homework/homework4/calc.h
@@ -5,4 +5,5 @@ int getop(char []);
 int getch(void);
 void ungetch(int);
 
-https://github.com/satuelisa/C/tree/main/Ch4
+/* this program was made with the help of the book Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), 
+https://github.com/satuelisa/C/tree/main/Ch4 and with the help of rulgamer03 */
