0fb0e56a04134c3ce10f28ac1572ba4bfec0c012
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-13 15:37:12 -0500

    Update main.c

--- homework/homework4/main.c
+++ homework/homework4/main.c
@@ -204,8 +204,11 @@ int main()
      default:
      error++;
      break;
-     }
-     }
     }
-    return 0;
+   }
+  }
+ return 0;
 }
+
+/* this program was made with the help of the book Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), 
+https://github.com/satuelisa/C/tree/main/Ch4 and with the help of rulgamer03 */
