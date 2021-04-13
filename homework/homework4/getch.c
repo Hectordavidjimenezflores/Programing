87d4d46a80f7992d3aef9909fdac980735f9631c
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-13 15:35:32 -0500

    Update getch.c

--- homework/homework4/getch.c
+++ homework/homework4/getch.c
@@ -14,3 +14,6 @@ void ungetch(int c)
     else
         buf[bufp++] = c;
 }
+
+/* this program was made with the help of the book Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), 
+https://github.com/satuelisa/C/tree/main/Ch4 and with the help of rulgamer03 */
