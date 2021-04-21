6f00e8016887ce1744aa0cc3cbe2c7569840b580
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-21 16:06:31 -0500

    Update calc.h

--- homework/homework5/calc.h
+++ homework/homework5/calc.h
@@ -2,7 +2,7 @@
 int getop (char[]);
 int getch (void);
 void ungetch (int);
-void showcompletematrix(int** , int );
-void showmatrixwhileplay(int** , int , int** );
+void showcompletematrix(int** , int);
+void showmatrixwhileplay(int** , int , int**);
 int contar(int** , int , int , int );
-int descubrir(int** , int** , int , int , int );
+int descubrir(int** , int** , int , int , int);
