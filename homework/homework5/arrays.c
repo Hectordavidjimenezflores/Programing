8d65e8abb002d6133448ea7680b3ffba72a5103d
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-21 18:23:20 -0500

    Update arrays.c

--- homework/homework5/arrays.c
+++ homework/homework5/arrays.c
@@ -81,18 +81,18 @@ void showmatrixwhileplay(int** m, int dim, int** d){
 }
     
 
-int contar(int** m, int tam, int a, int b){
+int count(int** m, int tam, int a, int b){
     int i, j;
-    int copia[tam][tam];
+    int copy[tam][tam];
     for (i = 0; i < tam; i++) {
     for (j = 0; j < tam; j++) {
-      copia[i][j]=m[i][j];
+      copy[i][j]=m[i][j];
       }
     }
   int x, y, z=0;
   for (x=a-1; x<3+a-1; x++){
       for (y=b-1;y<3+b-1;y++){
-          if (x>=0&&x<tam&&y>=0&&y<tam&&copia[x][y]==-1){
+          if (x>=0&&x<tam&&y>=0&&y<tam&&copy[x][y]==-1){
               z++;
     }
    }
@@ -101,122 +101,122 @@ int contar(int** m, int tam, int a, int b){
   
 }
 
-int descubrir(int** matrix, int** descubiertas, int tam, int numero_encontrado, int letra)
+int discover(int** array, int** discovered, int tam, int found number, int letter)
 {
-    if(matrix[numero_encontrado-1][letra] == -1){
+    if(array[found number-1][letter] == -1){
         return -1;
     }
-    else if(descubiertas[numero_encontrado-1][letra] == 1){
+    else if(discovered[found number-1][letter] == 1){
         return 0;
     }
     else{
         
-        if(matrix[numero_encontrado-1][letra] == 0){
+        if(array[found number-1][letter] == 0){
           
-            descubiertas[numero_encontrado-1][letra] = 1;
-            int resultado = 1;
+            discovered[found number-1][letter] = 1;
+            int result = 1;
 
            
             {
-                int fila = numero_encontrado - 1;
-                int columna = letra - 1;
+                int row = found number - 1;
+                int column = letter - 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
            
             {
-                int fila = numero_encontrado;
-                int columna = letra - 1;
+                int row = found number;
+                int column = letter - 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado + 1;
-                int columna = letra - 1;
+                int row = found number + 1;
+                int column = letter - 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado + 1;
-                int columna = letra;
+                int row = found number + 1;
+                int column = letter;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado + 1;
-                int columna = letra + 1;
+                int row = found number + 1;
+                int column = letter + 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado;
-                int columna = letra + 1;
+                int row = found number;
+                int column = letter + 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado - 1;
-                int columna = letra + 1;
+                int row = found number - 1;
+                int column = letter + 1;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
             
             {
-                int fila = numero_encontrado - 1;
-                int columna = letra;
+                int row = found number - 1;
+                int column = letter;
 
-                if ((fila - 1) >= 0 && (fila - 1) < tam && 
-                    columna >= 0 && columna < tam
+                if ((row - 1) >= 0 && (row - 1) < tam && 
+                    column >= 0 && column < tam
                 ){
-                    resultado += descubrir(matrix, descubiertas, tam, fila, columna);
+                    result += discover(array, discovered, tam, row, column);
               }
             }
 
-            return resultado;
+            return result;
         }
         else{
             
-         descubiertas[numero_encontrado-1][letra] = 1;
+         discovered[found number-1][letter] = 1;
           return 1;
     }
   }
