ab3e9a1f00e45688ee7b606719e48d27a97f6305
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-21 15:52:08 -0500

    Create calc.h

---
+++ homework/homework5/calc.h
@@ -0,0 +1,14 @@
+#define NUMBER '0'
+int getop (char[]);
+
+int getch (void);
+
+void ungetch (int);
+
+void showcompletematrix(int** , int );
+
+void showmatrixwhileplay(int** , int , int** );
+
+int contar(int** , int , int , int );
+
+int descubrir(int** , int** , int , int , int );
