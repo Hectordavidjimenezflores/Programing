1cb4f42d9854022c9c10865d5e92a165a3bfc9d4
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-04-21 21:36:20 -0500

    Update calc.h

--- homework/homework5/calc.h
+++ homework/homework5/calc.h
@@ -6,3 +6,5 @@ void showcompletematrix(int** , int);
 void showmatrixwhileplay(int** , int , int**);
 int count(int** , int , int , int );
 int discover(int** , int** , int , int , int);
+/* This program was made with the help of https://github.com/satuelisa/C/tree/main/Ch5, Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988),
+and with the help of rulgamer07 */
