c434f46461e94d88a3cab446098ac6509b0f09e8
Hectordavidjimenezflores <78566397+Hectordavidjimenezflores@users.noreply.github.com>
2021-05-06 11:28:26 -0500

    Update registros.h

--- homework/homework7/registros.h
+++ homework/homework7/registros.h
@@ -8,5 +8,5 @@ void registrar();
 void entradaosalida(int, int);
 void header();
 void horario();
-/* Este programa fue hecho con la ayuda de rulgamer07, Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), 
+/* Este programa fue hecho con la ayuda de rulgamer07, Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988), and
 https://github.com/satuelisa/C/tree/main/Ch7 */
